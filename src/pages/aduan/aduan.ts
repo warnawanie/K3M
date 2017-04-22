7ab6ad2086224f0fd74269f544fc62e10c987f5b
Hatta Zainal <kerismelayu@gmail.com>
2017-04-22 15:20:28 +0800

    Initial commit for aduan attachment view

--- src/pages/aduan/aduan.ts
+++ src/pages/aduan/aduan.ts
@@ -1,23 +1,38 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { Platform, ActionSheetController } from 'ionic-angular';
+// import { NavController, NavParams } from 'ionic-angular';
+// import { Platform, ActionSheetController } from 'ionic-angular';
+import { NavController, ActionSheetController, ToastController, Platform, LoadingController, Loading, NavParams } from 'ionic-angular';
 import { AduanSendPage } from '../aduan-send/aduan-send';
 import { Report } from '../../app/shared/sdk/models';
 import { ReportApi } from '../../app/shared/sdk/services';
 import { Customer}  from '../../app/shared/sdk/models';
 import { CustomerApi }  from '../../app/shared/sdk/services';
 import { TranslateService } from 'ng2-translate';
+import { Camera, File, Transfer, FilePath } from 'ionic-native';
+
+declare var cordova: any;
 
 @Component({
   selector: 'page-aduan',
   templateUrl: 'aduan.html'
 })
+
 export class AduanPage {
 
   public aduan: Report = new Report();
   aduanData:any;
+  lastImage: string = null;
+  loading: Loading;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public platform: Platform,public actionsheetCtrl: ActionSheetController, private reportApi: ReportApi, private customerApi: CustomerApi, public translateService: TranslateService) {
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    public platform: Platform,
+    public toastCtrl: ToastController,
+    public actionsheetCtrl: ActionSheetController,
+    public loadingCtrl: LoadingController, 
+    private reportApi: ReportApi, 
+    private customerApi: CustomerApi, 
+    public translateService: TranslateService) {
 
     this.customerApi.getCurrent().subscribe(
       
@@ -64,21 +79,23 @@ export class AduanPage {
         
         {
           text: 'Camera',
-          icon: !this.platform.is('ios') ? 'share' : null,
+          // icon: !this.platform.is('ios') ? 'share' : null,
           handler: () => {
             console.log('Camera clicked');
+            this.takePicture(Camera.PictureSourceType.CAMERA);
           }
         },
         {
           text: 'Album',
-          icon: !this.platform.is('ios') ? 'arrow-dropright-circle' : null,
+          // icon: !this.platform.is('ios') ? 'arrow-dropright-circle' : null,
           handler: () => {
             console.log('Album clicked');
+            this.takePicture(Camera.PictureSourceType.PHOTOLIBRARY);
           }
         },
         {
           text: 'Video',
-          icon: !this.platform.is('ios') ? 'heart-outline' : null,
+          // icon: !this.platform.is('ios') ? 'heart-outline' : null,
           handler: () => {
             console.log('Video clicked');
           }
@@ -86,7 +103,7 @@ export class AduanPage {
         {
           text: 'Cancel',
           role: 'cancel', // will always sort to be on the bottom
-          icon: !this.platform.is('ios') ? 'close' : null,
+          // icon: !this.platform.is('ios') ? 'close' : null,
           handler: () => {
             console.log('Cancel clicked');
           }
@@ -96,6 +113,104 @@ export class AduanPage {
     actionSheet.present();
   }
 
+  public takePicture(sourceType) {
+    // Create options for the Camera Dialog
+    var options = {
+      quality: 100,
+      sourceType: sourceType,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    };
+   
+    // Get the data of an image
+    Camera.getPicture(options).then((imagePath) => {
+      // Special handling for Android library
+      if (this.platform.is('android') && sourceType === Camera.PictureSourceType.PHOTOLIBRARY) {
+        FilePath.resolveNativePath(imagePath)
+        .then(filePath => {
+            let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+            let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
+          this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+        });
+      } else {
+        var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+        var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+        this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+      }
+    }, (err) => {
+      this.presentToast('Error while selecting image.');
+    });
+  }
+
+  // Create a new name for the image
+  private createFileName() {
+    var d = new Date(),
+    n = d.getTime(),
+    newFileName =  n + ".jpg";
+    return newFileName;
+  }
+   
+  // Copy the image to a local folder
+  private copyFileToLocalDir(namePath, currentName, newFileName) {
+    File.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
+      this.lastImage = newFileName;
+    }, error => {
+      this.presentToast('Error while storing file.');
+    });
+  }
+   
+  private presentToast(text) {
+    let toast = this.toastCtrl.create({
+      message: text,
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
+  }
+   
+  // Always get the accurate path to your apps folder
+  public pathForImage(img) {
+    if (img === null) {
+      return '';
+    } else {
+      return cordova.file.dataDirectory + img;
+    }
+  }
+
+  public uploadImage() {
+    // Destination URL
+    var url = "http://yoururl/upload.php";
+   
+    // File for Upload
+    var targetPath = this.pathForImage(this.lastImage);
+   
+    // File name only
+    var filename = this.lastImage;
+   
+    var options = {
+      fileKey: "file",
+      fileName: filename,
+      chunkedMode: false,
+      mimeType: "multipart/form-data",
+      params : {'fileName': filename}
+    };
+   
+    const fileTransfer = new Transfer();
+   
+    this.loading = this.loadingCtrl.create({
+      content: 'Uploading...',
+    });
+    this.loading.present();
+   
+    // Use the FileTransfer to upload the image
+    fileTransfer.upload(targetPath, url, options).then(data => {
+      this.loading.dismissAll()
+      this.presentToast('Image succesful uploaded.');
+    }, err => {
+      this.loading.dismissAll()
+      this.presentToast('Error while uploading file.');
+    });
+  }
  
 
 
