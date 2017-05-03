3cec85fa02dfbc87a9505b30e588904fc35b4634
Hatta Zainal <kerismelayu@gmail.com>
2017-05-03 16:30:08 +0800

    Fixed issues with attachment

--- src/pages/aduan/aduan.ts
+++ src/pages/aduan/aduan.ts
@@ -8,7 +8,9 @@ import { ReportApi } from '../../app/shared/sdk/services';
 import { Customer}  from '../../app/shared/sdk/models';
 import { CustomerApi, StorageApi }  from '../../app/shared/sdk/services';
 import { TranslateService } from 'ng2-translate';
-import { Camera, File, Transfer, FilePath } from 'ionic-native';
+import { Camera, FilePath } from 'ionic-native';
+import { Transfer, FileUploadOptions, TransferObject } from '@ionic-native/transfer';
+import { File } from '@ionic-native/file';
 import { LoopBackConfig } from '../../app/shared/sdk/lb.config';
 
 declare var cordova: any;
@@ -23,6 +25,7 @@ export class AduanPage {
   public aduan: Report = new Report();
   aduanData:any;
   lastImage: string = null;
+  serverFilename: string = null;
   loading: Loading;
 
   constructor(public navCtrl: NavController, 
@@ -34,7 +37,10 @@ export class AduanPage {
     private reportApi: ReportApi, 
     private customerApi: CustomerApi,
     private storageApi: StorageApi, 
-    public translateService: TranslateService) {
+    public translateService: TranslateService,
+    private transfer: Transfer, 
+    private file: File
+    ) {
 
     this.customerApi.getCurrent().subscribe(
       
@@ -63,6 +69,7 @@ export class AduanPage {
    report() {
    // report.name = customer.fullname
     this.reportApi.create(this.aduan).subscribe((aduan: Report) => {
+        this.aduan = aduan;
         // if attachment is available - upload image
         if(this.lastImage){
           this.uploadImage();
@@ -131,7 +138,7 @@ export class AduanPage {
       quality: 100,
       sourceType: sourceType,
       saveToPhotoAlbum: false,
-      destinationType: Camera.DestinationType.FILE_URI,
+      // destinationType: Camera.DestinationType.FILE_URI,
       correctOrientation: true
     };
    
@@ -165,7 +172,7 @@ export class AduanPage {
    
   // Copy the image to a local folder
   private copyFileToLocalDir(namePath, currentName, newFileName) {
-    File.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
+    this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
       this.lastImage = newFileName;
     }, error => {
       this.presentToast('Error while storing file.');
@@ -202,20 +209,20 @@ export class AduanPage {
     var filename = this.lastImage;
     var _token = this.customerApi.getCurrentToken();
 
-    console.log( _token );
+    console.log( url );
+    console.log( _token.id );
+
     var options = {
       fileKey: "file",
       fileName: filename,
       chunkedMode: false,
       mimeType: "multipart/form-data",
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': _token
-      },
-      params : {'fileName': filename}
+        'Authorization': _token.id
+      }
     };
    
-    const fileTransfer = new Transfer();
+    const fileTransfer: TransferObject = this.transfer.create();
    
     this.loading = this.loadingCtrl.create({
       content: 'Uploading...',
@@ -224,13 +231,43 @@ export class AduanPage {
    
     // Use the FileTransfer to upload the image
     fileTransfer.upload(targetPath, url, options).then(data => {
-      this.loading.dismissAll()
-      // this.presentToast('Image succesful uploaded.');
-      this.onGoToAduanSend();
+      console.log(data);
+      // save filename to aduan
+      var _response = JSON.parse( data.response );
+      console.log( _response );
+
+      this.serverFilename = _response.result.files.file[0].name;
+      console.log(this.serverFilename);
+
+      if(this.serverFilename){
+        this.updateReport();
+      }else{
+        this.loading.dismissAll()
+        this.onGoToAduanSend();
+      }
     }, err => {
+      console.log(err);
       this.loading.dismissAll()
+      this.onGoToAduanSend();
       this.presentToast('Error while uploading file.');
     });
+  }
+
+  private updateReport(){
+    // report.name = customer.fullname
+    this.reportApi.patchAttributes(this.aduan.id, {"file": this.serverFilename }).subscribe((aduan: Report) => {
+        console.log("filename updated");
+        console.log(aduan);
+        this.loading.dismissAll()
+        this.onGoToAduanSend();
+      },
+      err => {
+          console.log(err);
+          console.log("Oops!");
+          
+          this.loading.dismissAll()
+          this.onGoToAduanSend();
+      });
   }
  
 
