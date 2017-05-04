05656bbcb70b2813047abb72a39a4f5c6d666e09
Mohd Hafiz Safie <feezixora@gmail.com>
2017-05-04 13:04:37 +0800

    Latest update

--- src/pages/talian/talian.ts
+++ src/pages/talian/talian.ts
@@ -8,8 +8,20 @@ import { TranslateService } from 'ng2-translate';
   templateUrl: 'talian.html'
 })
 export class TalianPage {
-
+ shownGroup = null;
   constructor(public navCtrl: NavController, public navParams: NavParams, public translateService: TranslateService) {}
+
+
+  toggleGroup(group) {
+    if (this.isGroupShown(group)) {
+        this.shownGroup = null;
+    } else {
+        this.shownGroup = group;
+    }
+    };
+    isGroupShown(group) {
+        return this.shownGroup === group;
+    };
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TalianPage');
